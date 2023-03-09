16f33c846e661d24337b5208ee01d76460269a77
caixw <xnotepad@gmail.com>
2023-03-09 14:29:18 +0800

    ci: 支持 go1.20

--- phrase.go
+++ phrase.go
@@ -13,33 +13,36 @@ type (
 	// LocaleStringer 本地化字符串的接口中
 	LocaleStringer interface {
 		// LocaleString 返回当前对象的本地化字符串
-		LocaleString(*message.Printer) string
+		LocaleString(*Printer) string
 	}
 
 	phrase struct {
-		key    message.Reference
+		key    Key
 		values []interface{}
 	}
 
 	localeError phrase
+
+	Key     = message.Reference
+	Printer = message.Printer
 )
 
 // Phrase 返回一段未翻译的语言片段
 //
 // key 和 val 参数与 golang.org/x/text/message.Printer.Sprintf 的参数相同。
 // 如果 val 也实现了 LocaleStringer 接口，则会先调用 val 的 LocaleString 方法再传递给 Sprintf。
-func Phrase(key message.Reference, val ...interface{}) LocaleStringer {
+func Phrase(key Key, val ...interface{}) LocaleStringer {
 	return phrase{key: key, values: val}
 }
 
 // Error 返回未翻译的错误对象
 //
 // 该对象同时实现了 LocaleStringer 接口。
-func Error(key message.Reference, val ...interface{}) error {
+func Error(key Key, val ...interface{}) error {
 	return &localeError{key: key, values: val}
 }
 
-func (p phrase) LocaleString(printer *message.Printer) string {
+func (p phrase) LocaleString(printer *Printer) string {
 	values := make([]interface{}, 0, len(p.values))
 	for _, value := range p.values {
 		if ls, ok := value.(LocaleStringer); ok {
@@ -55,6 +58,6 @@ func (p phrase) String() string { return p.LocaleString(defaultPrinter) }
 
 func (err *localeError) Error() string { return err.LocaleString(defaultPrinter) }
 
-func (err *localeError) LocaleString(p *message.Printer) string {
+func (err *localeError) LocaleString(p *Printer) string {
 	return phrase(*err).LocaleString(p)
 }
