bacff3294dd587249dfe174e32b4297afef7ff8a
caixw <xnotepad@gmail.com>
2024-10-14 09:22:27 +0800

    test: 优化测试代码

--- message/extract/extract_test.go
+++ message/extract/extract_test.go
@@ -23,121 +23,127 @@ func TestExtract(t *testing.T) {
 
 	// 全部是本地对象
 
-	o := &Options{
-		Language:  language.MustParse("zh-CN"),
-		Root:      "./testdata/locale",
-		Recursive: true,
-		WarnLog:   log,
-		InfoLog:   log,
-		Funcs: []string{
-			"github.com/issue9/localeutil/testdata/locale.String",
-			"github.com/issue9/localeutil/testdata/locale.Print",
-			"github.com/issue9/localeutil/testdata/locale.GPrinter.GPrint",
-			"github.com/issue9/localeutil/testdata/locale.Printer.Print",
-			"github.com/issue9/localeutil/testdata/locale.Interface.Printf",
-		},
-	}
-	l, err := Extract(context.Background(), o)
-	a.NotError(err).NotNil(l).
-		Equal(l.ID.String(), "zh-CN")
+	t.Run("testdata/locale", func(t *testing.T) {
+		o := &Options{
+			Language:  language.MustParse("zh-CN"),
+			Root:      "./testdata/locale",
+			Recursive: true,
+			WarnLog:   log,
+			InfoLog:   log,
+			Funcs: []string{
+				"github.com/issue9/localeutil/testdata/locale.String",
+				"github.com/issue9/localeutil/testdata/locale.Print",
+				"github.com/issue9/localeutil/testdata/locale.GPrinter.GPrint",
+				"github.com/issue9/localeutil/testdata/locale.Printer.Print",
+				"github.com/issue9/localeutil/testdata/locale.Interface.Printf",
+			},
+		}
+		l, err := Extract(context.Background(), o)
+		a.NotError(err).NotNil(l).
+			Equal(l.ID.String(), "zh-CN")
 
-	m := l.Messages
-	a.NotNil(m).
-		Length(sliceutil.Dup(m, func(m1, m2 message.Message) bool { return m1.Key == m2.Key }), 0). // 没有重复值
-		Length(m, 13)
+		m := l.Messages
+		a.NotNil(m).
+			Length(sliceutil.Dup(m, func(m1, m2 message.Message) bool { return m1.Key == m2.Key }), 0). // 没有重复值
+			Length(m, 13)
 
-	for _, mm := range m {
-		t.Log(mm.Key)
-	}
+		for _, mm := range m {
+			t.Log(mm.Key)
+		}
+		t.Log("\n\n")
+	})
 
 	// 单个 localeutil.Phrase
+	t.Run("localeutilx1", func(t *testing.T) {
+		o := &Options{
+			Root:    "./testdata",
+			WarnLog: log,
+			Funcs:   []string{"github.com/issue9/localeutil.Phrase"},
+		}
+		l, err := Extract(context.Background(), o)
+		a.NotError(err).NotNil(l)
 
-	o = &Options{
-		Root:    "./testdata",
-		WarnLog: log,
-		Funcs:   []string{"github.com/issue9/localeutil.Phrase"},
-	}
-	l, err = Extract(context.Background(), o)
-	a.NotError(err).NotNil(l)
+		m := l.Messages
+		a.Length(m, 4).
+			Length(sliceutil.Dup(m, func(m1, m2 message.Message) bool { return m1.Key == m2.Key }), 0) // 没有重复值
 
-	m = l.Messages
-	a.NotNil(m).
-		Length(sliceutil.Dup(m, func(m1, m2 message.Message) bool { return m1.Key == m2.Key }), 0). // 没有重复值
-		Length(m, 4)
-
-	for _, mm := range m {
-		t.Log(mm.Key)
-	}
+		for _, mm := range m {
+			t.Log(mm.Key)
+		}
+		t.Log("\n\n")
+	})
 
 	// 添加了 localeutil.Error 和 localeutil.StringPhrase，
+	t.Run("localeutilx3", func(t *testing.T) {
+		o := &Options{
+			Root:      "./testdata",
+			Recursive: true,
+			WarnLog:   log,
+			Funcs: []string{
+				"github.com/issue9/localeutil.Phrase",
+				"github.com/issue9/localeutil.Error",
+				"github.com/issue9/localeutil.StringPhrase",
+				//"github.com/issue9/localeutil/testdata.String", // 别名，指向 localeutil.StringPhrase
+			},
+		}
+		l, err := Extract(context.Background(), o)
+		a.NotError(err).NotNil(l).
+			NotNil(l)
 
-	o = &Options{
-		Root:      "./testdata",
-		Recursive: true,
-		WarnLog:   log,
-		Funcs: []string{
-			"github.com/issue9/localeutil.Phrase",
-			"github.com/issue9/localeutil.Error",
-			"github.com/issue9/localeutil.StringPhrase",
-			//"github.com/issue9/localeutil/testdata.String", // 别名，指向 localeutil.StringPhrase
-		},
-	}
-	l, err = Extract(context.Background(), o)
-	a.NotError(err).NotNil(l).
-		NotNil(l)
+		m := l.Messages
+		a.Length(m, 9).
+			Length(sliceutil.Dup(m, func(m1, m2 message.Message) bool { return m1.Key == m2.Key }), 0) // 没有重复值
 
-	m = l.Messages
-	a.NotNil(m).
-		Length(sliceutil.Dup(m, func(m1, m2 message.Message) bool { return m1.Key == m2.Key }), 0). // 没有重复值
-		Length(m, 9)
-
-	for _, mm := range m {
-		t.Log(mm.Key)
-	}
+		for _, mm := range m {
+			t.Log(mm.Key)
+		}
+		t.Log("\n\n")
+	})
 
 	// 添加了 locale.Printer.Print
+	t.Run("localeutilx3,localex2", func(t *testing.T) {
+		o := &Options{
+			Root:      "./testdata",
+			Recursive: true,
+			WarnLog:   log,
+			InfoLog:   log,
+			Funcs: []string{
+				"github.com/issue9/localeutil.Phrase",
+				"github.com/issue9/localeutil.Error",
+				"github.com/issue9/localeutil.StringPhrase",
+				"github.com/issue9/localeutil/testdata/locale.String",        // 别名，ref.String 指向此值
+				"github.com/issue9/localeutil/testdata/locale.Printer.Print", // ref.Printer 指向此值
+			},
+		}
+		l, err := Extract(context.Background(), o)
+		a.NotError(err).NotNil(l).
+			NotNil(l)
 
-	o = &Options{
-		Root:      "./testdata",
-		Recursive: true,
-		WarnLog:   log,
-		InfoLog:   log,
-		Funcs: []string{
-			"github.com/issue9/localeutil.Phrase",
-			"github.com/issue9/localeutil.Error",
-			"github.com/issue9/localeutil.StringPhrase",
-			"github.com/issue9/localeutil/testdata/locale.String",        // 别名，ref.String 指向此值
-			"github.com/issue9/localeutil/testdata/locale.Printer.Print", // ref.Printer 指向此值
-		},
-	}
-	l, err = Extract(context.Background(), o)
-	a.NotError(err).NotNil(l).
-		NotNil(l)
+		m := l.Messages
+		a.Length(m, 17)
 
-	m = l.Messages
-	a.NotNil(m).
-		Length(m, 17)
-
-	for _, mm := range m {
-		t.Log(mm.Key)
-	}
+		for _, mm := range m {
+			t.Log(mm.Key)
+		}
+		t.Log("\n\n")
+	})
 }
 
-func TestGetTypeName(t *testing.T) {
+func TestParseTypeName(t *testing.T) {
 	a := assert.New(t, false)
 
-	p, s := getTypeName("t")
+	p, s := parseTypeName("t")
 	a.Empty(p).Equal(s, "t")
 
-	p, s = getTypeName("*github.com/issue9/abc.t")
+	p, s = parseTypeName("*github.com/issue9/abc.t")
 	a.Equal(p, "github.com/issue9/abc").Equal(s, "t")
 
-	p, s = getTypeName("github.com/issue9/abc.t[int]")
+	p, s = parseTypeName("github.com/issue9/abc.t[int]")
 	a.Equal(p, "github.com/issue9/abc").Equal(s, "t")
 
-	p, s = getTypeName("*github.com/issue9/abc.t[github.com/issue9/abc.Type]")
+	p, s = parseTypeName("*github.com/issue9/abc.t[github.com/issue9/abc.Type]")
 	a.Equal(p, "github.com/issue9/abc").Equal(s, "t")
 
-	p, s = getTypeName("github.com/issue9/abc.t[github.com/issue9/abc.Type]")
+	p, s = parseTypeName("github.com/issue9/abc.t[github.com/issue9/abc.Type]")
 	a.Equal(p, "github.com/issue9/abc").Equal(s, "t")
 }
